a7d191cd659a5241895758a6f02fc8b97e0807ff
eacevedof <eacevedof@yahoo.es>
2014-12-16 17:02:34 +0100

    @file server.js @version: 1.0.1
    Cambios en funciones _on

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @author Eduardo Acevedo Farje.
  * @link: www.eduardoaf.com
  * @file server.js 
- * @version: 1.0.0
+ * @version: 1.0.1
  * @name: 
- * @date: 16-12-2014 16:36 (SPAIN)
+ * @date: 16-12-2014 17:01 (SPAIN)
  * @observations: core library.
  * @requires:
  */
@@ -18,12 +18,12 @@ var oUrl = require("url");
 var oUtils = require("./the_framework/utils");
 var oConfig = require("./the_framework/config");
         
-var fn_on_geterror = function(oError)
+var fn_ongeterror = function(oError)
 {
     oUtils.bug("There was an error: "+oError.message);
 }
 
-var fn_on_getresponse = function(oResponse)
+var fn_ongetresponse = function(oResponse)
 {
     if(oResponse.statusCode==200)
     {
@@ -35,11 +35,11 @@ var fn_on_getresponse = function(oResponse)
     }
 }
 
-var fn_on_request = function(oRequest,oResponse)
+var fn_onrequest = function(oRequest,oResponse)
 {
     var sMessage = "this is a content!!";
     var oOptions = {host: "www.eduardoaf.com",port:"80",path:"/"};
-    oHttp.get(oOptions,fn_on_getresponse).on("error",fn_on_geterror);
+    oHttp.get(oOptions,fn_ongetresponse).on("error",fn_ongeterror);
     
     //oUtils.bug(oRequest);
     oResponse.writeHead(200,{"Content-Type":"text/plain"});
@@ -47,7 +47,7 @@ var fn_on_request = function(oRequest,oResponse)
 };
 
 //En cada petición
-oHttp.createServer(fn_on_request).listen(oConfig.get_port(),oConfig.get_ip());
+oHttp.createServer(fn_onrequest).listen(oConfig.get_port(),oConfig.get_ip());
 
 console.log("processid:"+process.pid);
 console.log("Server running at: "+oConfig.get_ip()+":"+oConfig.get_port());

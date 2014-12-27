e70735861afe63668f730a1a124dcde86b7f77c4
eduardoaf.com <eacevedof@yahoo.es>
2014-12-27 13:56:35 +0100

    @version: 1.0.6
    Configuración de servidor por setters

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @author Eduardo Acevedo Farje.
  * @link: www.eduardoaf.com
  * @file index.js 
- * @version: 1.0.5
+ * @version: 1.0.6
  * @name: 
- * @date: 27-12-2014 13:11 (SPAIN)
+ * @date: 27-12-2014 13:51 (SPAIN)
  * @observations: core library.
  *      Archivo principal. Equivalente a index.html
  * @requires:
@@ -21,10 +21,16 @@ var oUtils = require("./the_framework/components/component_utils");
 var oConfig = require("./the_framework/components/component_config");
 var oServer = require("./the_framework/components/component_server");
 
-//Configuro la ip y el puerto de escucha del servidor node
+//Configuro mi objeto servidor
+//Objetos iniciales
+oServer.set_objhttp(oHttp);
+oServer.set_objurl(oUrl);
+oServer.set_objfs(oFs);
+//datos de escucha
 oServer.set_ip(oConfig.get_ip());
 oServer.set_port(oConfig.get_port());
-oServer.init(oHttp,oUrl,oFs);
+//inicializa el servidor
+oServer.init();
 
-console.log("processid:"+process.pid);
-console.log("Server running at: "+oConfig.get_ip()+":"+oConfig.get_port());
+console.log(":0) Your NODE server is running at: "+oConfig.get_ip()+":"+oConfig.get_port());
+console.log("Using processid:"+process.pid);

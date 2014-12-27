7c03047c00497ff89de1fc3bdb8e3fed1c64de0b
eduardoaf.com <eacevedof@yahoo.es>
2014-12-27 14:24:33 +0100

    @version: 1.0.8
    Pruebas cambiando oUrl a oUrlX para ver si se rompia la visibilidad de fn_oncreateeserver. No, no se rompe.
    Pruebas con parametro 2 de oUrl.parse.
    true y false.  Parece que tiene que ver con que se devuelva los parámetros en json o en un string key=value

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @author Eduardo Acevedo Farje.
  * @link: www.eduardoaf.com
  * @file index.js 
- * @version: 1.0.7
+ * @version: 1.0.8
  * @name: 
- * @date: 27-12-2014 14:07 (SPAIN)
+ * @date: 27-12-2014 14:22 (SPAIN)
  * @observations: core library.
  *      Archivo principal. Equivalente a index.html
  * @requires:
@@ -31,12 +31,20 @@ oServer.set_ip(oConfig.get_ip());
 oServer.set_port(oConfig.get_port());
 
 //Función que lee parametros get
+//Nota: esta función tiene acceso a oUrl que no es el atributo de oServer sino la variable de módulo definida
+//en este archivo. De aqui deduzco que, como todo lo que hay dentro de set_oncreateserer tiene visibilidad sobre
+//las variables de este archivo sus argumentos tambien los tienen. Por lo tanto fn_oncreateserver puede trabajar
+//con oUrl. @TODO Habría que hacer una prueba empirica
 function fn_oncreatesever(oRequest,oResponse)
 { 
-   var oQuery = oUrl.parse(oRequest.url,true).query;
-   var sModule = oQuery.module;
-   oResponse.writeHead(200, {'Content-Type': 'text/html'});
-   oResponse.end(sModule);
+    //con true: {module:'accounts'}
+    var jnParams = oUrl.parse(oRequest.url,true).query;
+    //con false: oQuery es module=accounts
+    oUtils.bug(jnParams,"oQuery");
+  
+    var sModule = jnParams.module;
+    oResponse.writeHead(200, {"Content-Type":"text/html"});
+    oResponse.end(sModule);
 }
 
 oServer.set_oncreateserver(fn_oncreatesever);

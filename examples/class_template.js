d5b707666955da093da0c633cce73690c92de350
eacevedof <eacevedof@yahoo.es>
2014-12-30 09:46:13 +0100

    Template

--- examples/class_template.js
+++ examples/class_template.js
@@ -4,33 +4,34 @@
  * @file class_template.js 
  * @version: 1.0.0
  * @name: Template
- * @date: 27-12-2014 15:02 (SPAIN)
+ * @date: 00-00-0000 00:00 (SPAIN)
  * @observations: 
  * @repo: https://github.com/eacevedof/prj_nodejs/
  * @requires:
  */
 var Template = function()
 {
-    var sId = "";
-    var sName = "";
-    var arObjOptions = [];
+    var _prop1 = "";
+    var _prop2 = "";
+    var _prop3 = [];
 
     this.init = function(){;}
-
 
     //===========================
     //        SETTERS
     //===========================
-    this.set_id = function(sValue){sId = sValue;}
-    this.set_name = function(iValue){sName = iValue;}
-    this.set_options = function(arObjValues){arObjOptions = arObjValues}
+    this.set_prop1 = function(sValue){_prop1 = sValue;}
+    this.set_prop2 = function(iValue){ _prop2 = iValue;}
+    this.set_prop3 = function(arObjValues){_prop3 = arObjValues;}
 
     //===========================
     //        GETTERS
     //===========================    
-    this.get_id = function(){return sId;}
-    this.get_name = function(){return iPort;}    
-}
+    this.get_prop1 = function(){return _prop1;}
+    this.get_prop2 = function(){return _prop2;}
+    this.get_prop3 = function(){return _prop3;}
+    
+}//Template
 
 var oExport = new Template();
 module.exports = oExport;
